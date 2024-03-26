b28913ae56fea00fd59a4c33b860492183b70230
me <me@nothankyou.com>
2024-03-25 17:12:29 -0700

    working workspaces widget

--- config.js
+++ config.js
@@ -9,22 +9,24 @@ const date = Variable("", {
   poll: [1000, 'date "+%H:%M:%S %b %e."'],
 })
 
-// widgets can be only assigned as a child in one container
-// so to make a reuseable widget, make it a function
-// then you can simply instantiate one by calling it
+//Workspaces Widget (mostly complete)
 
 function Workspaces() {
-  const activeId = hyprland.active.workspace.bind("id")
-  const workspaces = hyprland.bind("workspaces")
-    .as(ws => ws.map(({ id }) => Widget.Button({
-      on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-      child: Widget.Label(`${id}`),
-      class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
-    })))
-
-  return Widget.Box({
+  return Widget.EventBox({
     class_name: "workspaces",
-    children: workspaces,
+    child: Widget.Box({
+      children: Array.from({ length: 10 }, (_, i) => i + 1).map(i => Widget.Button({
+        attribute: i,
+        label: `${i}`,
+        onClicked: () => hyprland.messageAsync(`dispatch workspace ${i}`),
+        class_name: hyprland.active.workspace.bind("id").as(id => `${id === i ? "focused" : ""}`),
+      })),
+
+      // remove this setup hook if you want fixed number of buttons
+      setup: self => self.hook(hyprland, () => self.children.forEach(btn => {
+        btn.visible = hyprland.workspaces.some(ws => ws.id === btn.attribute);
+      })),
+    }),
   })
 }
 
